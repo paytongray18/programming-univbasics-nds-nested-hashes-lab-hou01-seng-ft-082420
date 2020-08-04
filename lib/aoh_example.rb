04454afaab8de0c894a95ceda47b62b4e0acbcf7
Payton Gray <paytongray18@gmail.com>
2020-08-04 16:26:44 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -7,11 +7,7 @@ KATE_G = { name:  "Kate Gompert", occupation: "None" }
 BRUCE_G = { name:  "Bruce Green", occupation: "Fan of Mildred" }
 
 def assembled_aoh
-assembled_aoh = [{ :name=> "Don Gately", :occupation=> "Live-in Staff" } 
-{ :name=>  "Joelle van Dyne", :occupation=> "Radio Personality" }
-{ :name=>  "Pat Monteseian", :occupation=> "Staff" }
-{ :name=>  "Kate Gompert", :occupation=> "None" }
-{ :name=>  "Bruce Green", :occupation=> "Fan of Mildred" }]
+assembled_aoh = [DON_G, JOELLE_VD, PAT_M, KATE_G, BRUCE_G]
 end
 
 def literal_aoh
